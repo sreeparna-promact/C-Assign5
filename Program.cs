13f4f7ec962504de5038c2bc7abcbe6be84e6186
Sreeparna das <sreeparna14@gmail.com>
2019-02-12 11:03:23 +0530

    commited

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> primeMinisters = new Dictionary<int, string>();
+            primeMinisters.Add(1998, "Atal Bihari Vajpayee");
+            primeMinisters.Add(2014, "Narendra Modi");
+            primeMinisters.Add(2004, "Manmohan Singh");
+
+            
+
+
+
+                if (primeMinisters.ContainsKey(2004))
+                {
+                    Console.WriteLine("The Prime Minister of 2004 is:" + primeMinisters[2004]);
+                }
+
+            primeMinisters.Add(2019, "Narendra Modi");
+
+            var list = primeMinisters.Keys.ToList();
+            list.Sort();
+            foreach (var keys in list)
+            {
+                Console.WriteLine("{0}:{1}",keys,primeMinisters[keys]);
+            }   
+
+
+        }
+    }
+}
